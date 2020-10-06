48a769445ef5d6f4be85059c30575ea560f80454
Ankur Agarwal <ankur.a.agarwal@gmail.com>
2020-10-06 14:30:11 -0700

    Refactor to avoid finding the boundary

--- search_rotated.py
+++ search_rotated.py
@@ -3,45 +3,25 @@
 import unittest
 t = unittest.TestCase()
 def binary_search_rotated(arr, key):
-      
+
   def binary_search(arr, low, high, key):
-    if low > high:
+    if low > high or (low == high and arr[low] != key):
       return -1
     mid = (low + high) // 2
-    if arr[mid] > key:
+#    print(low, high, key, mid)
+    if arr[mid] == key:
+      return mid
+    elif arr[mid] < arr[high]: # if RHS is sorted
+      if arr[mid] < key <= arr[high]: # key lies in sorted part
+        return binary_search(arr, mid+1, high, key)
       return binary_search(arr, low, mid-1, key)
-    elif arr[mid] < key:
+    elif arr[low] < arr[mid]: # if LHS is sorted
+      if arr[low] <= key < arr[mid]: # key lies in sorted part
+        return binary_search(arr, low, mid-1, key)
       return binary_search(arr, mid+1, high, key)
-    else:
-      return mid
 
-  def find_boundary(arr, low, high):
-    if low == high:
-      return low 
-    mid = (low + high) // 2
-    if arr[mid] > arr[mid+1]:
-      return mid
-    elif arr[mid] < arr[mid-1]: 
-      return mid
-    elif arr[mid] < arr[low]: # boundary is on the left hand side
-      return find_boundary(arr, low, mid-1)
-    elif arr[mid] > arr[high]: # boundary is on the right hand side
-      return find_boundary(arr, mid+1, high)
-    else:
-       pass # control should not reach here
-
-  boundary_idx = find_boundary(arr, 0, len(arr) - 1)
-  if arr[boundary_idx] > arr[boundary_idx + 1]:
-     low1, high1, low2, high2 = 0, boundary_idx, boundary_idx + 1, len(arr) - 1
-  else:
-     low1, high1, low2, high2 = 0, boundary_idx-1, boundary_idx, len(arr) - 1
-
-  mid = binary_search(arr, low1, high1, key)
-  if mid == -1:
-    return binary_search(arr, low2, high2, key)
-  else:
-    return mid
-
+  return binary_search(arr, 0, len(arr)-1, key)
+  
 l = [176,188,199,200,210,222,1,10,20,47,59,63,75,88,99,107,120,133,155,162]
 t.assertEqual(binary_search_rotated(l , 210), 4)
 
@@ -50,3 +30,7 @@ t.assertEqual(binary_search_rotated(l , 8),1)
 
 l = [4, 5, 6, 1, 2, 3]
 t.assertEqual(binary_search_rotated(l , 6),2)
+t.assertEqual(binary_search_rotated(l , 3),5)
+
+l = [176,188,199,200,210,222,1,10,20,47,59,63,75,88,99,107,120,133,155,162]
+t.assertEqual(binary_search_rotated(l , 211), -1)
