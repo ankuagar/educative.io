e5dcb7595e83c843bcff4bcd421d04bf889994fd
Ankur Agarwal <ankur.a.agarwal@gmail.com>
2020-10-06 21:17:56 -0700

    Fix a bug
    Compare key for equality with arr[low] and arr[high] when
    determing if it lies in the sorted half

--- search_rotated.py
+++ search_rotated.py
@@ -3,25 +3,25 @@
 import unittest
 t = unittest.TestCase()
 def binary_search_rotated(arr, key):
-
-  def binary_search(arr, low, high, key):
- #   print(low, high, key, end=" ")
+  low, high = 0, len(arr) - 1
+  while True:
+    # print(low, high, key, end=" ")
     if low > high or (low == high and arr[low] != key):
       return -1
     mid = (low + high) // 2
-#    print(mid)
+    # print(mid)
     if arr[mid] == key:
       return mid
     elif arr[mid] < arr[high]: # if RHS is sorted
       if arr[mid] < key <= arr[high]: # key lies in sorted part
-        return binary_search(arr, mid+1, high, key)
-      return binary_search(arr, low, mid-1, key)
+        low, high = mid+1, high
+        continue
+      low, high = low, mid-1
     elif arr[low] < arr[mid]: # if LHS is sorted
       if arr[low] <= key < arr[mid]: # key lies in sorted part
-        return binary_search(arr, low, mid-1, key)
-      return binary_search(arr, mid+1, high, key)
-
-  return binary_search(arr, 0, len(arr)-1, key)
+        low, high = low, mid-1
+        continue
+      low, high = mid+1, high
   
 l = [176,188,199,200,210,222,1,10,20,47,59,63,75,88,99,107,120,133,155,162]
 t.assertEqual(binary_search_rotated(l , 210), 4)
